ff3d3803ec0bc259860e7d2e0ba2ba5f98f8cf6a
Evan Hsu <evanhsu@gmail.com>
2022-04-09 18:58:28 -0600

    Create photo-archive page, link to Flickr

--- src/pages/photo-archive/index.tsx
+++ src/pages/photo-archive/index.tsx
@@ -1,11 +1,20 @@
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import React from 'react';
+import styles from './index.module.css';
 
 const PhotoArchivePage = (props) => {
   return (
     <Layout title="Photos" description="Reunion Home Base">
       <main>
-        <section>Photo Archive Page</section>
+        <div className={styles.centeredBlock}>
+          <section className={styles.column}>
+            <h1>Ancestral Photo Archive</h1>
+            <Link href="https://www.flickr.com/groups/14817766@N21/pool/">
+              Flickr Gallery
+            </Link>
+          </section>
+        </div>
       </main>
     </Layout>
   );
